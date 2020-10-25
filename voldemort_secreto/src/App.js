30dabe6b1f1c65da5c2c1d775944068b104c6e28
Agustin_M_Dominguez <agustin.marcelo.dominguez@mi.unc.edu.ar>
2020-10-24 22:13:32 -0300

    Experimentos Raros para armar componentes despues - @shirosweets Agregar un icono y cambiar su animacion // Agregar un formulario para registro // Agregamos un boton que loguea en consola

--- voldemort_secreto/src/App.js
+++ voldemort_secreto/src/App.js
@@ -1,25 +1,58 @@
-import logo from './logo.svg';
+//import logo from './logo.svg';
+import nimbus from './nimbus.svg';
 import './App.css';
+
+const doSomething = function doSomething ()
+{
+  console.log("Clicked!")
+}
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={nimbus} className="App-logo" alt="logo" />
+        <img src={nimbus} className="App-logo2" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Bienvenido a Secret Voldemort 
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://drive.google.com/drive/folders/1Pr_bbW787_3N7bLAmw2CrIMbEZedu-vp"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Doc Principal
         </a>
+        <p></p>
+        <button onClick={doSomething} >Click me</button>
+        <textarea>
+          esto no
+        </textarea>
+
+        <textarea>
+          esto tampoco
+        </textarea>
+        <form action="/my-handling-form-page" method="get">
+          <ul>
+            <li>
+              <label for="name">Username: </label>
+              <input></input>
+              <button onClick={doSomething} >Check Username</button>
+            </li>
+            <li>
+              <label for="name">Password: </label>
+              <input></input>
+              <button onClick={doSomething} >Check Password</button>
+            </li>
+            <button onClick={doSomething} >Login</button>
+          </ul>
+        </form>
       </header>
     </div>
   );
 }
 
+
+// <form action="/my-handling-form-page" method="post">
 export default App;
