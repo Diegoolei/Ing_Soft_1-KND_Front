b77685d8bc4b23ab8db26e1e51328f777c0f90d2
Agustin_M_Dominguez <agustin.marcelo.dominguez@mi.unc.edu.ar>
2020-10-25 12:19:36 -0300

    Movi los experimentos de ayer en un componente appheader.js

--- voldemort_secreto/src/App.js
+++ voldemort_secreto/src/App.js
@@ -1,58 +1,17 @@
-//import logo from './logo.svg';
-import nimbus from './nimbus.svg';
-import './App.css';
+import { Provider } from 'react-redux'
+import store from  './redux/store'
 
-const doSomething = function doSomething ()
-{
-  console.log("Clicked!")
-}
+import AppHeader from './components/appheader'
+import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={nimbus} className="App-logo" alt="logo" />
-        <img src={nimbus} className="App-logo2" alt="logo" />
-        <p>
-          Bienvenido a Secret Voldemort 
-        </p>
-        <a
-          className="App-link"
-          href="https://drive.google.com/drive/folders/1Pr_bbW787_3N7bLAmw2CrIMbEZedu-vp"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Doc Principal
-        </a>
-        <p></p>
-        <button onClick={doSomething} >Click me</button>
-        <textarea>
-          esto no
-        </textarea>
-
-        <textarea>
-          esto tampoco
-        </textarea>
-        <form action="/my-handling-form-page" method="get">
-          <ul>
-            <li>
-              <label for="name">Username: </label>
-              <input></input>
-              <button onClick={doSomething} >Check Username</button>
-            </li>
-            <li>
-              <label for="name">Password: </label>
-              <input></input>
-              <button onClick={doSomething} >Check Password</button>
-            </li>
-            <button onClick={doSomething} >Login</button>
-          </ul>
-        </form>
-      </header>
+      <Provider store={store}>
+        <AppHeader />
+      </Provider>
     </div>
   );
 }
 
-
-// <form action="/my-handling-form-page" method="post">
 export default App;
