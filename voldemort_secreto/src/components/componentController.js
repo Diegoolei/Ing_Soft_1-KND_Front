197c517af1c444baac710c9932e9c46a2f00f6bd
Agustin_M_Dominguez <agustin.marcelo.dominguez@mi.unc.edu.ar>
2020-11-01 16:48:02 -0300

    IN28 Added End Msg Component - To be adapted with redux state of game

--- voldemort_secreto/src/components/componentController.js
+++ voldemort_secreto/src/components/componentController.js
@@ -2,11 +2,12 @@ import React from 'react'
 import { useSelector } from 'react-redux'
 import {
   LOGIN_COMPONENT, REGISTER_COMPONENT, MAIN_MENU_COMPONENT, CREATE_LOBBY_COMPONENT,
-  JOIN_LOBBY_COMPONENT,
+  JOIN_LOBBY_COMPONENT, END_GAME_MSG_COMPONENT
 } from '../redux/componentController/componentControllerTypes'
 import LoginForm from './loginForm'
 import RegisterForm from './registerForm'
 import MainMenu from './mainMenu'
+import EndGameMsg from './endGameMsg'
 
 function ComponentController() {
   const currentScreen = useSelector(state => state.controller.screen)
@@ -18,6 +19,7 @@ function ComponentController() {
       case MAIN_MENU_COMPONENT: return <MainMenu/>
       case CREATE_LOBBY_COMPONENT: return <div>Create Lobby Component not made yet</div>
       case JOIN_LOBBY_COMPONENT: return <div>Join Lobby Component not made yet</div>
+      case END_GAME_MSG_COMPONENT: return <EndGameMsg/>
     
       default: return (
         <div>
---
+++ voldemort_secreto/src/components/endGameMsg.js
@@ -0,0 +1,12 @@
+import React from 'react'
+//import { useSelector, useDispatch } from 'react-redux'
+
+function EndGameMsg () {
+  return (
+    <div>
+      <h1>The Game ended</h1>      
+    </div>
+  )
+}
+
+export default EndGameMsg
