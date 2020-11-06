774adf66cec21f9e44757f72ebf69448ac276f15
Agustin_M_Dominguez <agustin.marcelo.dominguez@mi.unc.edu.ar>
2020-11-06 16:12:11 -0300

    FIX IN75 - Now on login we send the request even though the password length is not the correct one

--- voldemort_secreto/src/App.js
+++ voldemort_secreto/src/App.js
@@ -1,15 +1,15 @@
 import { Provider } from 'react-redux'
 import store from  './redux/store'
-//import ComponentController from './components/componentController'
-import SocketTest from './components/socketTest'
+import ComponentController from './components/componentController'
+//import SocketTest from './components/socketTest'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <Provider store={store}>
-        {/* <ComponentController/> */}
-        <SocketTest/>
+        <ComponentController/>
+        {/* <SocketTest/> */}
       </Provider>
     </div>
   );
