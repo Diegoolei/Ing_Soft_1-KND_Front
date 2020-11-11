8262acd39963be1fa363ce82b31d407f43f3ae60
Agustin_M_Dominguez <agustin.marcelo.dominguez@mi.unc.edu.ar>
2020-11-11 15:51:53 -0300

    IN49 Moved functionality to Redux Store and cleaned up some of files

--- voldemort_secreto/src/redux/rootReducer.js
+++ voldemort_secreto/src/redux/rootReducer.js
@@ -2,11 +2,13 @@ import { combineReducers } from 'redux'
 import sessionReducer from './session/sessionReducer'
 import componentControllerReducer from './componentController/componentControllerReducer'
 import socketReducer from './socket/socketReducer'
+import lobbyGameListReducer from './lobbyGameList/lobbyGameListReducer'
 
 const rootReducer = combineReducers ({
   controller : componentControllerReducer,
   session : sessionReducer,
-  socket: socketReducer
+  socket: socketReducer,
+  joinlists: lobbyGameListReducer
 })
 
 export default rootReducer
