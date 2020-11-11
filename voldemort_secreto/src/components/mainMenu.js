89103b5cdacef5c8ddd14f823c179b40bc088b9a
Agustin_M_Dominguez <agustin.marcelo.dominguez@mi.unc.edu.ar>
2020-11-11 15:51:53 -0300

    IN20 Moved Current Functionality to Redux and cleaned up some of files

--- voldemort_secreto/src/components/mainMenu.js
+++ voldemort_secreto/src/components/mainMenu.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { useDispatch } from 'react-redux'
-import { logoutSuccess, changeScreen } from '../redux/reduxIndex'
+import { logoutSuccess, changeScreen, renderLobbyPage } from '../redux/reduxIndex'
 import { 
   LOGIN_COMPONENT,
   ENDPOINT_SOCKET_TEST_COMPONENT,
@@ -21,11 +21,17 @@ function MainMenu () {
   function data() {
     dispatch(changeScreen(UPDATE_PROFILE_COMPONENT))
   }
+
+  function joinlobby() {
+    dispatch(renderLobbyPage(1))
+    dispatch(changeScreen(JOIN_LOBBY_COMPONENT))
+  }
+
   return (
     <div>
       <h1>MAIN MENU</h1>
       <br/><button onClick={() => dispatch(changeScreen(CREATE_LOBBY_COMPONENT))}>Create Lobby</button>
-      <br/><button onClick={() => dispatch(changeScreen(JOIN_LOBBY_COMPONENT))}>Join Lobby</button>
+      <br/><button onClick={joinlobby}>Join Lobby</button>
       <br/><button onClick={data}>Update User Data</button>
       <br/><button >View History</button>
       <br/><button >Settings</button>
---
+++ voldemort_secreto/src/redux/lobbyGameList/lobbyGameListActions.js
@@ -0,0 +1,76 @@
+import axios from 'axios'
+import {
+  LG_LISTS_REQUEST_LOBBY_PAGE,
+  LG_LISTS_REQUEST_GAME_PAGE,
+  LG_LISTS_SET_LOBBY_LIST,
+  LG_LISTS_SET_GAME_LIST,
+  LG_LISTS_SET_RESPONSE
+} from './lobbyGameListTypes'
+import { BASE_URL ,API_ENDPOINT_LIST_LOBBIES } from '../API_Types'
+import store from '../store'
+
+
+export const requestLobbyPage = pageNumber => {
+  return {
+    type : LG_LISTS_REQUEST_LOBBY_PAGE,
+    payload : pageNumber
+  }
+}
+
+export const requestGamePage = pageNumber => {
+  return {
+    type : LG_LISTS_REQUEST_GAME_PAGE,
+    payload : pageNumber
+  }
+}
+
+export const setLobbyPage = response => {
+  return {
+    type: LG_LISTS_SET_LOBBY_LIST,
+    payload: response
+  }
+}
+
+export const setGamePage = response => {
+  return {
+    type: LG_LISTS_SET_GAME_LIST,
+    payload: response
+  }
+}
+
+export const setResponse = response => {
+  return {
+    type: LG_LISTS_SET_RESPONSE,
+    payload: response
+  }
+}
+
+export const renderLobbyPage = pageNumber => {
+  const state = store.getState()
+  const token = state.session.authToken
+  return dispatch => {
+    dispatch(requestLobbyPage(pageNumber))
+    axios.get(BASE_URL+API_ENDPOINT_LIST_LOBBIES,
+      {
+        headers: {'Authorization': token.token_type + " " + token.access_token}
+      }).then(response => {
+        const lobbies = response.data
+        const resp = {
+          content: lobbies,
+          entries: lobbies.lobbyDict.length,
+          date: Date.now()
+        }
+        dispatch(setLobbyPage(resp))
+        console.log("-Response :" + JSON.stringify(response.data.lobbyDict))
+      }).catch(error => {
+        let errorMsg
+        try {
+          errorMsg = error.response.data.detail
+        }
+        catch (er) {
+          errorMsg = "Something went wrong  " + JSON.stringify(er)
+        }
+        dispatch(setResponse(errorMsg))
+      })
+  }
+}
