4388045d770fb4832786f9c1fcbd9c3d111ae0a0
Agustin_M_Dominguez <agustin.marcelo.dominguez@mi.unc.edu.ar>
2020-11-13 16:06:52 -0300

    IN51 Join Lobby - Added partial dispatch Implementation

--- voldemort_secreto/src/redux/game/gameReducer.js
+++ voldemort_secreto/src/redux/game/gameReducer.js
@@ -1,6 +1,14 @@
+import {
+  CGL_SET_LOBBY_INFORMATION,
+  CGL_SET_GAME_IMFORMATION,
+  CGL_PLAYER_JOINED_LOBBY,
+  CGL_PROCLAIM_PHOENIX,
+  CGL_PROCLAIM_DEATH_EATER
+} from './gameTypes'
 
 const initialState = {
   lobby_id: 0,
+  lobby_name: '',
   game_id: 0,
   turn_step: -1,
   player_id: -1,
@@ -8,35 +16,58 @@ const initialState = {
   chat_blocked: false,
   current_minister: -1,
   current_director: -1,
-  player_array: [],
+  player_array: null,
   election_counter: 0,
   cards_in_deck: 17,
   proclaimed_phoenix: 0,
   proclaimed_death_eater: 0,
-  actions : [],
   loading: false
 }
 
-// Endpoint refresh state
+const getInitialPlayer = nick => {
+  return {
+    nick: nick,
+    player_number: -1,
+    connected: true,
+    role: -1,
+    is_alive: true,
+    is_candidate: false,
+    vote: -1
+  }
+}
 
-/*other_player = {
-  nick: str,
-  player_number: int,
-  connected: bool,
-  role: int,  // [UNKNOWN ... and the others],
-  is_alive: bool,
-  is_candidate: bool, 
-  vote: int
-}*/
+const gameReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case CGL_SET_LOBBY_INFORMATION: return {
+      ...state,
+      lobby_id: action.payload.lobby_id,
+      lobby_name: action.payload.lobby_name,
+      player_id: action.payload.player_id
+    }
+      
+    case CGL_SET_GAME_IMFORMATION: return state
 
-/*
-Todos los jugadores
-- si está muerto //*DONE
-- nombre //*DONE
-- si es director, ministro o candidato a director //*DONE
-- si fue el anterior ministro o director //? MAYBE..
-- ver orden de jugador //*DONE
-- Mortífagos: saben quiénes son entre ellos //*DONE
-- "que acción hizo" (votos) //?DUNNO
-- si está conectado //*DONE
-*/
+    case CGL_PLAYER_JOINED_LOBBY: return {
+      ...state,
+      player_array: {
+        ...state.player_array,
+        [action.payload]: getInitialPlayer(action.payload)
+      }
+    }
+
+    case CGL_PROCLAIM_PHOENIX: return {
+      ...state,
+      proclaimed_phoenix: state.proclaimed_phoenix + 1
+    }
+    
+    case CGL_PROCLAIM_DEATH_EATER: return {
+      ...state, 
+      proclaimed_death_eater: state.proclaimed_death_eater + 1
+    }
+  
+    default:  return state
+
+  }
+}
+
+export default gameReducer
