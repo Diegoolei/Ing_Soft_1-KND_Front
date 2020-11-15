14d716ef45fad38c5fa1c1368a56d7fd5be96e6b
Agustin_M_Dominguez <agustin.marcelo.dominguez@mi.unc.edu.ar>
2020-11-14 22:40:10 -0300

    IN5X Minor format fixes

--- voldemort_secreto/src/components/game.js
+++ voldemort_secreto/src/components/game.js
@@ -2,15 +2,14 @@ import React from 'react'
 import { useDispatch } from 'react-redux'
 import { joinGame } from '../redux/reduxIndex'
 
-function Lobby () {
+function Game () {
   const dispatch = useDispatch()
   
   return (
     <div>
       <h1>GAME</h1>
-      
     </div>
   )
 }
 
-export default Lobby
+export default Game
