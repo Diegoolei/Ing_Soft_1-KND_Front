8d49b931dc8219f9039d70dc9004e51e5e145204
knd9 <candela.spitale@mi.unc.edu.ar>
2020-11-21 17:06:14 -0300

    redux skeleton

--- voldemort_secreto/src/redux/game/votationResults/votationResultsActions.js
+++ voldemort_secreto/src/redux/game/votationResults/votationResultsActions.js
@@ -1,2 +1,18 @@
 
-// Cande agrega aca
+import {
+  APPS_ACTIVATE_VOTATION_RESULTS,
+  APPS_DESACTIVE_VOTATION_RESULTS
+} from './votationResultsReducer'
+
+export const activateShowResults = () => {
+  return {
+    type: APPS_ACTIVATE_VOTATION_RESULTS,
+  }
+}
+
+export const deactivateShowResults = () => {
+  return {
+    type: APPS_DESACTIVE_VOTATION_RESULTS,
+  }
+}
+
--- voldemort_secreto/src/redux/game/votationResults/votationResultsReducer.js
+++ voldemort_secreto/src/redux/game/votationResults/votationResultsReducer.js
@@ -1,2 +1,27 @@
 
-// Cande agrega aca
+import {
+  APPS_ACTIVATE_VOTATION_RESULTS,
+  APPS_DESACTIVE_VOTATION_RESULTS
+} from './activeAppsTypes'
+
+  const initialState = {
+    is_show_results_active: false
+  }
+ 
+  const activeAppsReducer = (state = initialState, action) => {
+    switch (action.type) {
+      case APPS_ACTIVATE_VOTATION_RESULTS: return {
+        ...state,
+        is_show_results_active: true
+      }
+
+      case APPS_DESACTIVE_VOTATION_RESULTS: return {
+        ...state,
+        is_show_results_active: false
+      }
+
+      default: return state
+    }
+  }
+
+  export default activeAppsReducer
